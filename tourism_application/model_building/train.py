45fe566c60afd5022f7555eaa583fa1e3881f98b
vyasmax9-cloud <vyasmax9@gmail.com>
2026-04-30 21:18:16 +0530

    Update train.py

--- tourism_application/model_building/train.py
+++ tourism_application/model_building/train.py
@@ -46,13 +46,11 @@ param_grid = {
     'xgbclassifier__reg_lambda': [0.4, 0.5, 0.6],
 }
 
-# Model pipeline
-model_pipeline = make_pipeline(preprocessor, xgb_model)
 
 # Start MLflow run
 with mlflow.start_run():
     # Hyperparameter tuning
-    grid_search = GridSearchCV(model_pipeline, param_grid, cv=5, n_jobs=-1)
+    grid_search = GridSearchCV(xgb_model, param_grid, cv=5, n_jobs=-1)
     grid_search.fit(Xtrain, ytrain)
 
     # Log all parameter combinations and their mean test scores
